8694d4611fb2ec082a23ab9ef16f95579d0dbd92
Kyle <kyle.c.r.fahey@gmail.com>
2017-07-21 14:04:50 -0500

    Enable Coors, Update Callback

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,34 @@ const spotifyApi = new Spotify({
   redirectUri: REDIRECT_URI
 });
 
-
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (_, res) => {
+  const state = generateRandomString(16);
+  res.cookie(STATE_KEY, state);
+  res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
+});
+
+router.get('/callback', (req, res) => {
+ const { code, state } = req.query;
+ const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+ if (state === null || state !== storedState) {
+    res.redirect('/#/error/state mismatch');
+  } else {
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+      const { expires_in, access_token, refresh_token } = data.body;
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+      res.redirect(`/#/user/${access_token}/${refresh_token}`);
+    });
+  }
+  }).catch(err => {
+    res.redirect('/#/error/invalid token');
+  });
+
 
 
 
